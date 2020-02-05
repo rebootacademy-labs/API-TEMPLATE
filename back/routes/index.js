6952a450cb33c50f6fead42f52dc42781d37a6fb
Fer Martin <fernandoMartinSantana@gmail.com>
2020-02-05 18:40:31 +0000

    deleted backend

--- back/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const homesRouter = require('./homes.router');
-const usersRouter = require('./users.router');
-
-router.use('/homes', homesRouter);
-router.use('/users', usersRouter);
-
-module.exports = router;
